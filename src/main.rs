6822531d80be343219678b0585ffb29cdbf5ddb8
Denis <daerexq@yande.ru>
2025-10-27 00:20:31 +0300

    Add main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
 
+use std::io::{self, Read};
+use std::fs::File;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Не удалось считать строку");
+    match File::open(input.trim()) {
+        Ok(mut f) => {
+            let mut buf = Vec::new();
+            if f.read_to_end(&mut buf).is_ok() {
+                println!("success");
+            } else {
+                println!("failure");
+            }
+        }
+        Err(_) => {
+            println!("failure");
+        }
+    } 
 }
